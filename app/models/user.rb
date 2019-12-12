a2eebeefa1311ac05b949f716ae009308820788d
nakano <aiueo.candy3726@gmail.com>
2019-12-12 13:23:34 +0000

    rails06 kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
     validates :password, length: {minimum: 8, maximum: 32 }, format: {with: VALID_PASSWORED_REGEX}
     
-    
     has_secure_password
     
+    has_many :topics
 end
